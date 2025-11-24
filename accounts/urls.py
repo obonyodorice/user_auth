7bdfdb1ad324d5de5be7df8e600efab851c4ecb3
obonyodorice <obonyodorice3@gmail.com>
2025-11-24 13:57:33 -0800

    cm

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,12 +2,9 @@ from django.urls import path
 from .views import (
     UserRegistrationView,
     UserLoginView,
-    UserLogoutView,
     ChangePasswordView,
     PasswordResetRequestView,
     PasswordResetConfirmView,
-    EmailVerificationView,
-    ResendVerificationEmailView,
     UserDetailView,
     DeleteAccountView,
     UserListView,
@@ -18,14 +15,11 @@ app_name = 'authentication'
 urlpatterns = [
     path('register/', UserRegistrationView.as_view(), name='register'),
     path('login/', UserLoginView.as_view(), name='login'),
-    path('logout/', UserLogoutView.as_view(), name='logout'),
 
     path('change-password/', ChangePasswordView.as_view(), name='change-password'),
     path('password-reset/request/', PasswordResetRequestView.as_view(), name='password-reset-request'),
     path('password-reset/confirm/', PasswordResetConfirmView.as_view(), name='password-reset-confirm'),
    
-    path('verify-email/', EmailVerificationView.as_view(), name='verify-email'),
-    path('resend-verification/', ResendVerificationEmailView.as_view(), name='resend-verification'),
    
     path('user/', UserDetailView.as_view(), name='user-detail'),
     path('user/delete/', DeleteAccountView.as_view(), name='delete-account'),
